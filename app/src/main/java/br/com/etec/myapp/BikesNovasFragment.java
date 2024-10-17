d8dba0921a56d66c8183656c47869c658d1ccff3
katia.beloto <katia.beloto@hotmail.com>
2024-10-17 20:18:59 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etec/myapp/BikesNovasFragment.java
@@ -0,0 +1,24 @@
+package br.com.etec.myapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class BikesNovasFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_bikes_novas, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/br/com/etec/myapp/RecyclerAdapterBikeNova.java
@@ -0,0 +1,65 @@
+package br.com.etec.myapp;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecyclerAdapterBikeNova extends RecyclerView.Adapter<RecyclerAdapterBikeNova.ViewHolder> {
+//criando variáveis globais para representar a lista e contexto
+    private Context context;
+    private List<BikeNova>lstBikeNova;
+
+    //criando o construtor
+    public RecyclerAdapterBikeNova(Context context, List<BikeNova> lstBikeNova) {
+        this.context = context;
+        this.lstBikeNova = lstBikeNova;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        view = inflater.inflate(R.layout.modelo_bikes_novas,parent,false);
+        return null;
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position)   {
+holder.idModeloTituloBikeNova.setText(lstBikeNova.get(position).getTitulo());
+holder.idModeloImagemBikeNova.setImageResource(lstBikeNova.get(position).getImagem());
+    }
+
+    @Override
+    public int getItemCount() {
+        return lstBikeNova.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        //Criando variável que irá representar o xml no java
+        CardView idModeloBikeNova;
+        ImageView idModeloImagemBikeNova;
+        TextView idModeloTituloBikeNova;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            //Apresentando o java para o xml do modelo
+            idModeloBikeNova = itemView.findViewById(R.id.idModeloBikeNova);
+            idModeloTituloBikeNova = itemView.findViewById(R.id.idModeloTituloBikeNova);
+            idModeloImagemBikeNova = itemView.findViewById(R.id.idModeloImagemBikeNova);
+        }
+    }
+
+}
