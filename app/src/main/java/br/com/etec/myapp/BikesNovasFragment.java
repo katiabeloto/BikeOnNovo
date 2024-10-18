c86b154961d599e1e8dd1bfd495db1600016238f
katia.beloto <katia.beloto@hotmail.com>
2024-10-17 22:15:40 -0300

    card descricao

--- app/src/main/java/br/com/etec/myapp/BikesNovasFragment.java
+++ app/src/main/java/br/com/etec/myapp/BikesNovasFragment.java
@@ -3,14 +3,20 @@ package br.com.etec.myapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class BikesNovasFragment extends Fragment {
-
+private List <BikeNova>lstBikeNova;
+RecyclerView idRecBikesNovas;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -18,7 +24,26 @@ public class BikesNovasFragment extends Fragment {
         // Inflate the layout for this fragment
 
         View view = inflater.inflate(R.layout.fragment_bikes_novas, container, false);
+        idRecBikesNovas = view.findViewById(R.id.idRecBikesNovas);
 
+        // Carregando os objetos na lista
+        lstBikeNova = new ArrayList<>();
+
+        lstBikeNova.add(new BikeNova("vintage",R.drawable.bikea));
+        lstBikeNova.add(new BikeNova("vintage",R.drawable.bikeb));
+        lstBikeNova.add(new BikeNova("vintage",R.drawable.bikec));
+        lstBikeNova.add(new BikeNova("vintage",R.drawable.biked));
+        lstBikeNova.add(new BikeNova("vintage",R.drawable.bikee));
+        lstBikeNova.add(new BikeNova("vintage",R.drawable.bikef));
+        lstBikeNova.add(new BikeNova("vintage",R.drawable.bikeg));
+        lstBikeNova.add(new BikeNova("vintage",R.drawable.bikeh));
+        lstBikeNova.add(new BikeNova("vintage",R.drawable.bikei));
+        lstBikeNova.add(new BikeNova("vintage",R.drawable.bikej));
+
+        //carregando o adaptador
+        RecyclerAdapterBikeNova adapterBikeNova = new RecyclerAdapterBikeNova(getContext(),lstBikeNova);
+        idRecBikesNovas.setLayoutManager(new GridLayoutManager(getContext(),2));
+        idRecBikesNovas.setAdapter(adapterBikeNova);
         return view;
     }
 }
